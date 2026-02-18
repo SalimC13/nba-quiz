f07263f9030e28a9d7c9fe66cba0068cf3655bae
SalimC13 <sbcox@cps.edu>
2026-02-18 13:15:04 -0600

    Add functionality to determine G.O.A.T based on input

--- script.js
+++ script.js
@@ -1,1 +1,31 @@
+let input1 = document.querySelector(".answerOne");
+let input2 = document.querySelector(".answerTwo");
+let button = document.querySelector("button");
 
+button.onclick=function(){
+   console.log("button check");
+ let answerOne = input1.value;
+ let answerTwo = input2.value;
+ let jordanImg = document.querySelector(".jordan-img");
+ let h1 = document.querySelector(".heading");
+  
+  if(answerOne >= 35 && answerTwo === "bigs"){
+    jordanImg.src= "reem.jpg";
+    h1.innerHTML="Your G.O.A.T is Kareem Abdul-Jabbar";
+    }
+    else if(answerOne <= 35 && answerTwo === "bigs"){
+      jordanImg.src="shaq.jpg";
+      h1.innerHTML="Your G.O.A.T is Shaquille O'Neal";
+    } 
+    else if(answerOne <= 35 && answerTwo === "guards"){
+      jordanImg.src="bron.jpg";
+      h1.innerHTML="Your G.O.A.T is LeBron James";
+    }
+    else if(answerOne >= 35 && answerTwo === "guards"){
+      jordanImg.src="jordan.jpg";
+      h1.innerHTML="Your G.O.A.T is Michael Jordan";
+    }
+    else{
+      alert("Wrong input")
+    }
+ };
